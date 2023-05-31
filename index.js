7dc8b50e93affe1ea2077c0bfdbd1d3a32783601
trissentety <trissentety@gmail.com>
2023-05-31 16:26:01 -0700

    First commit

--- index.js
+++ index.js
@@ -20,9 +20,18 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', (req, res) => {
+  //res.send(console.log(req.headers))
+  const ipaddress = req.headers.host
+  const language = req.headers["accept-language"]
+  const software = req.headers["user-agent"]
+  res.json({
+    ipaddress,
+    language,
+    software
+  })
+})
+    
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
